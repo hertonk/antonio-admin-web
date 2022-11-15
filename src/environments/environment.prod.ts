2af6ec4136bbd8e49e6631f1459b33a18d95bfc0
Herton Vilarim <hertonk@gmail.com>
2022-11-15 12:24:46 -0300

    First Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2021-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'https://appagro.online/api/', // ex https://yourapi.com/public/api/ don't forgot to add public/api/ at the end
+  imageUrl: 'https://appagro.online/api/storage/images/', // ex https://yourapi.com/public/storage/images/ don't forgot to add public/storage/images at the end
+};
